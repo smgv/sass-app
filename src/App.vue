021f6a7414b3be12110e2f61a0604ebd309b6bfa
smgv <gpatra1996@gmail.com>
2024-04-04 19:43:09 +0530

    refactored

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
-<script setup lang="ts">
-import Home from "@/components/Home.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h1 class="text-3xl underline text-red-500">Hello world!</h1>
-  <Home />
 </template>
 
 <style scoped></style>
