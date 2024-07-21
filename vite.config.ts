c9e90b41f85952ea17af5781fa1ccc35f43f1c68
smgv <gpatra1996@gmail.com>
2024-07-21 21:15:09 +0530

    vercel buidl

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
 import { fileURLToPath } from "node:url";
 import { dirname, resolve } from "node:path";
-
-// import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig, loadEnv } from "vite";
 import viteTsconfigPaths from "vite-tsconfig-paths";
-
 import pkg from "./package.json";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const { VITE_BUILD_ENV } = loadEnv(mode, process.cwd());
-
   const version = `${pkg.version}`;
   const isProduction = ["production", "staging"].includes(VITE_BUILD_ENV);
   process.env.VITE_BUILD_VERSION = isProduction ? version : "";
@@ -32,21 +27,10 @@ export default defineConfig(({ mode }) => {
       __SENTRY_DEBUG__: false,
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
     },
-    plugins: [
-      vue(),
-      viteTsconfigPaths(),
-      // VueI18nPlugin({
-      //   /* options */
-      //   // locale messages resource pre-compile option
-      //   include: resolve(dirname(fileURLToPath(import.meta.url)), "./src/plugins/i18n/**"),
-      //   runtimeOnly: true,
-      // }),
-    ],
+    plugins: [vue(), viteTsconfigPaths()],
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
-        // buffer: resolve("./node_modules/buffer"),
-        // lodash: resolve("./node_modules/lodash-es"),
       },
     },
   };
